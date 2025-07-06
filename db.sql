d5f86de1a033a586717ee0a2a4a374a98308ab0b
Thinh <heyiamthinh1003@gmail.com>
2025-07-06 14:42:54 +0700

    Added sql script

---
+++ db.sql
@@ -0,0 +1,24 @@
+drop database if exists todoapp;
+
+create database if not exists todoapp;
+use todoapp;
+
+create table if not exists `user`
+(
+	id int auto_increment primary key,
+    username varchar(20) not null,
+    `password` varchar(200) not null,
+    email varchar(100) not null
+);
+
+create table if not exists task
+(
+	id int auto_increment primary key,
+    user_id int not null,
+    title varchar(200) not null,
+    `description` varchar(200),
+    deadline datetime not null,
+    is_completed boolean not null default false,
+    deleted_at datetime default null,
+    foreign key (user_id) references `user`(id)
+);
