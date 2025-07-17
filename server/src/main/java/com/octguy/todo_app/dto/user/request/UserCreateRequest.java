78b699aca8bb580cc60095af178c56cd05c79cbd
Thinh <heyiamthinh1003@gmail.com>
2025-07-17 14:43:14 +0700

    feat: add validation for username and password fields in UserCreateRequest

--- server/src/main/java/com/octguy/todo_app/dto/user/request/UserCreateRequest.java
+++ server/src/main/java/com/octguy/todo_app/dto/user/request/UserCreateRequest.java
@@ -3,6 +3,7 @@ package com.octguy.todo_app.dto.user.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.Setter;
 public class UserCreateRequest {
 
     @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
 
     @NotBlank(message = "Email is required")
@@ -22,6 +24,7 @@ public class UserCreateRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Pattern(
         regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])(?=.*[A-Z]).*$",
         message = "Password must contain at least 1 number, 1 special character, and 1 uppercase letter"
